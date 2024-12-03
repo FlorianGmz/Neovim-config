5b2197b8eaed882c23d503ea54422f60db40bd73
FlorianGmz <florian.g.code@gmail.com>
2024-12-03 22:51:20 +0100

    Yazi vs Neotree

--- lua/flo/plugins/auto-session.lua
+++ lua/flo/plugins/auto-session.lua
@@ -5,7 +5,7 @@ return {
     auto_restore_enabled = false,
     auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "~/Documents", "~/Desktop/" },
   }
-  
+      
   local keymap = vim.keymap -- for conciseness
   keymap.set('n', '<leader>wr', '<cmd>SessionRestore<CR>', { desc = 'Restore session for cwd' })
   keymap.set('n', '<leader>ws', '<cmd>SessionSave<CR>', { desc = 'Save session for autosession root dir' })
